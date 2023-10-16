b87c03681eb9b75afa276ed1ff1d66fde3728c7a
willvernon <wmvernon@proton.me>
2023-10-16 13:17:36 -0400

    nav setup

---
+++ components/Menubar.tsx
@@ -0,0 +1,110 @@
+import {
+	Menubar,
+	MenubarCheckboxItem,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarRadioGroup,
+	MenubarRadioItem,
+	MenubarSeparator,
+	MenubarShortcut,
+	MenubarSub,
+	MenubarSubContent,
+	MenubarSubTrigger,
+	MenubarTrigger,
+} from '@/components/ui/menubar'
+
+export default function MenubarMain() {
+	return (
+		<Menubar className='rounded-xl'>
+			<MenubarMenu>
+				<MenubarTrigger>File</MenubarTrigger>
+				<MenubarContent className='bg-slate-300 rounded-xl'>
+					<MenubarItem>
+						New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+					</MenubarItem>
+					<MenubarItem>
+						New Window <MenubarShortcut>⌘N</MenubarShortcut>
+					</MenubarItem>
+					<MenubarItem disabled>New Incognito Window</MenubarItem>
+					<MenubarSeparator />
+					<MenubarSub>
+						<MenubarSubTrigger>Share</MenubarSubTrigger>
+						<MenubarSubContent className='bg-slate-300 rounded-xl'>
+							<MenubarItem>Email link</MenubarItem>
+							<MenubarItem>Messages</MenubarItem>
+							<MenubarItem>Notes</MenubarItem>
+						</MenubarSubContent>
+					</MenubarSub>
+					<MenubarSeparator />
+					<MenubarItem>
+						Print... <MenubarShortcut>⌘P</MenubarShortcut>
+					</MenubarItem>
+				</MenubarContent>
+			</MenubarMenu>
+			<MenubarMenu>
+				<MenubarTrigger>Edit</MenubarTrigger>
+				<MenubarContent className='bg-slate-300 rounded-xl'>
+					<MenubarItem>
+						Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+					</MenubarItem>
+					<MenubarItem>
+						Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+					</MenubarItem>
+					<MenubarSeparator />
+					<MenubarSub>
+						<MenubarSubTrigger>Find</MenubarSubTrigger>
+						<MenubarSubContent className='bg-slate-300 rounded-xl'>
+							<MenubarItem>Search the web</MenubarItem>
+							<MenubarSeparator />
+							<MenubarItem>Find...</MenubarItem>
+							<MenubarItem>Find Next</MenubarItem>
+							<MenubarItem>Find Previous</MenubarItem>
+						</MenubarSubContent>
+					</MenubarSub>
+					<MenubarSeparator />
+					<MenubarItem>Cut</MenubarItem>
+					<MenubarItem>Copy</MenubarItem>
+					<MenubarItem>Paste</MenubarItem>
+				</MenubarContent>
+			</MenubarMenu>
+			<MenubarMenu>
+				<MenubarTrigger>View</MenubarTrigger>
+				<MenubarContent className='bg-slate-300 rounded-xl'>
+					<MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
+					<MenubarCheckboxItem checked>
+						Always Show Full URLs
+					</MenubarCheckboxItem>
+					<MenubarSeparator />
+					<MenubarItem inset>
+						Reload <MenubarShortcut>⌘R</MenubarShortcut>
+					</MenubarItem>
+					<MenubarItem
+						disabled
+						inset
+					>
+						Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+					</MenubarItem>
+					<MenubarSeparator />
+					<MenubarItem inset>Toggle Fullscreen</MenubarItem>
+					<MenubarSeparator />
+					<MenubarItem inset>Hide Sidebar</MenubarItem>
+				</MenubarContent>
+			</MenubarMenu>
+			<MenubarMenu>
+				<MenubarTrigger>Profiles</MenubarTrigger>
+				<MenubarContent className='bg-slate-300 rounded-xl'>
+					<MenubarRadioGroup value='benoit'>
+						<MenubarRadioItem value='andy'>Andy</MenubarRadioItem>
+						<MenubarRadioItem value='benoit'>Benoit</MenubarRadioItem>
+						<MenubarRadioItem value='Luis'>Luis</MenubarRadioItem>
+					</MenubarRadioGroup>
+					<MenubarSeparator />
+					<MenubarItem inset>Edit...</MenubarItem>
+					<MenubarSeparator />
+					<MenubarItem inset>Add Profile...</MenubarItem>
+				</MenubarContent>
+			</MenubarMenu>
+		</Menubar>
+	)
+}
